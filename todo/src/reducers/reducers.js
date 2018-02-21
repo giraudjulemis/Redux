5f500da040bbcaf9ae56c45c47accec6742fb0e6
Giraud Julemis <giraud.julemis@gmail.com>
2018-02-21 13:25:40 -0500

    split todos and todosapp function

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -5,25 +5,36 @@ const initialState = {
   todos: []
 }
 
+todos = (state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, completed: false}]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+}
+
 todoApp = (state = initialState, action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return { ...state, visibilityFilter: action.filter };
     case ADD_TODO:
-      return { ...state,
-        todos:[...state.todos, { text: action.text, completed: false }]
-      };
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      });
     case TOGGLE_TODO:
-      return { ...state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return { ...todo, {
-              completed: !todo.completed
-            }};
-          }
-          return todo;
-        });
-      }};
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      });
     default:
         return state;
   }
