a992f27a5bbed59d266871d1a5baff334728c9be
Giraud Julemis <giraud.julemis@gmail.com>
2018-02-21 13:04:32 -0500

    defined reducers

---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,30 @@
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions';
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+todoApp = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return { ...state, visibilityFilter: action.filter };
+    case ADD_TODO:
+      return { ...state,
+        todos:[...state.todos, { text: action.text, completed: false }]
+      };
+    case TOGGLE_TODO:
+      return { ...state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return { ...todo, {
+              completed: !todo.completed
+            }};
+          }
+          return todo;
+        });
+      }};
+    default:
+        return state;
+  }
+}
