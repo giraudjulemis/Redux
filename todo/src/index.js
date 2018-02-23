0774240ac68afcd8728236298729362c8d52a25e
Giraud Julemis <giraud.julemis@gmail.com>
2018-02-23 10:59:25 -0500

    working state

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,7 +6,30 @@ import todoApp from './reducers/reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './actions/actions';
+
 let store = createStore(todoApp);
+
+// Log the initial state
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+unsubscribe();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
