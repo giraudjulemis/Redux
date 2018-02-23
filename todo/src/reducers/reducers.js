826757de45da439837809d59fe3c35a76919bef0
Giraud Julemis <giraud.julemis@gmail.com>
2018-02-23 10:02:50 -0500

    split todos and todosapp

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -5,40 +5,39 @@ const initialState = {
   todos: []
 };
 
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo;
+      })
+      break;
+    default:
+        return state;
+  }
+};
+
 const todoApp = (state = initialState, action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
-      // return Object.assign({}, state, {
-      //   visibilityFilter: action.filter
-      // });
       return { ...state, visibilityFilter: action.filter };
     case ADD_TODO:
-      // return Object.assign({}, state, {
-      //   todos: [
-      //     ...state.todos,
-      //     {
-      //       text: action.text,
-      //       completed: false
-      //     }
-      //   ]
-      // });
-      return {
-        ...state,
-        todos: [
-          ...state.todos, { text: action.text, completed: false }
-        ]
-      };
+      return { ...state, todos: todos(state.todos, action) };
     case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            })
-          }
-          return todo;
-        })
-      });
+      return { ...state, todos: todos(state.todos, action) };
     default:
       return state;
   }
